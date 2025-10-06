ce0aeaf495c21804203208cc69b4acda63a559b9
Jung-Kwon Lee <73648662+leejk0924@users.noreply.github.com>
2025-10-06 19:12:49 +0900

    init: 프로젝트 세팅

---
+++ service/like/src/main/java/com/jk/board/like/LikeApplication.java
@@ -0,0 +1,11 @@
+package com.jk.board.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(com.jk.board.like.LikeApplication.class, args);
+    }
+}
